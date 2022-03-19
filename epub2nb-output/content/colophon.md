e783ae7a66ca1cb5433166859ea00267122cb767
adelaweidischova <48671822+adelaweidischova@users.noreply.github.com>
2022-03-19 11:49:54 +0000

    Deploying to docs from @ books-are-next/dracula@34261634dbb6b4f9d18de0b4b3cefb4350d8d755 🚀

--- epub2nb-output/content/colophon.md
+++ epub2nb-output/content/colophon.md
@@ -2,3 +2,76 @@
 title: Tiráž
 ---
 
+***
+
+Dracula
+
+Bram Stoker
+
+_Přeložil Tomáš Korbař_
+
+
+***
+
+Znění tohoto textu vychází z díla [Dracula](https://search.mlp.cz/cz/titul/dracula/2246778/#/) tak, jak bylo vydáno nakladatelstvím Odeon v Praze v roce 1970. Pro potřeby vydání Městské knihovny v Praze byl text redakčně zpracován.
+
+**§**
+
+Text díla (Bram Stoker: Dracula), publikovaného [Městskou knihovnou v Praze](https://www.mlp.cz/cz/), je vázán autorskými právy a jeho použití je definováno [Autorským zákonem](https://www.mkcr.cz/predpisy-zakonu-709.html) č. 121/2000 Sb.
+
+![image001.jpg](./resources/image001_fmt.jpeg)
+
+Vydání (obálka, upoutávka, citační stránka a grafická úprava), jehož autorem je Městská knihovna v Praze, podléhá licenci [Creative Commons Uveďte autora-Nevyužívejte dílo komerčně-Zachovejte licenci 3.0 Česko](https://creativecommons.org/licenses/by-nc-sa/3.0/cz/).
+
+Verze 1.0 z 13. 3. 2020.
+
+  
+  
+
+![image002.jpg](./resources/image002_fmt.jpeg)
+
+
+***
+
+**Bram Stoker  
+Dracula**
+
+  
+
+Překlad Tomáš Korbař
+
+  
+
+Ilustrace na obálce z volných zdrojů
+
+  
+
+Redakce Jaroslava Bednářová
+
+  
+
+Vydala Městská knihovna v Praze
+
+  
+
+Mariánské nám. 1, 115 72 Praha 1
+
+  
+
+V MKP 1. elektronické vydání
+
+  
+
+Verze 1.0 z 13. 3. 2020
+
+  
+
+ISBN 978-80-274-0461-2 (epub)
+
+  
+
+ISBN 978-80-274-0462-9 (pdf)
+
+  
+
+ISBN 978-80-274-0463-6 (prc)
--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 
 /* global self, caches, fetch */
 
-const CACHE = 'cache-1222772';
+const CACHE = 'cache-3426163';
 
 self.addEventListener('install', e => {
   e.waitUntil(precache()).then(() => self.skipWaiting());
@@ -40,7 +40,7 @@ self.addEventListener('activate', event => {
 });
 
 function precache() {
-  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./dracula_001.html","./dracula_003.html","./dracula_004.html","./dracula_002.html","./dracula_005.html","./dracula_006.html","./dracula_007.html","./dracula_008.html","./dracula_009.html","./dracula_010.html","./dracula_011.html","./dracula_012.html","./dracula_013.html","./dracula_014.html","./dracula_015.html","./dracula_016.html","./dracula_017.html","./dracula_018.html","./dracula_019.html","./dracula_020.html","./dracula_021.html","./dracula_022.html","./dracula_023.html","./dracula_024.html","./dracula_025.html","./dracula_026.html","./dracula_027.html","./dracula_028.html","./dracula_029.html","./dracula_030.html","./dracula_031.html","./dracula_032.html","./dracula_033.html","./dracula_034.html","./dracula_035.html","./favicon.png","./index.html","./manifest.json","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/image001_fmt.jpeg","./resources/image002_fmt.jpeg","./resources/obalka_dracula_fmt.jpeg","./resources/upoutavka_eknihy_fmt.jpeg","./scripts/bundle.js","./style/style.min.css","./template-images/circles.png"]));
+  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./dracula_002.html","./dracula_005.html","./dracula_006.html","./dracula_007.html","./dracula_008.html","./dracula_009.html","./dracula_010.html","./dracula_011.html","./dracula_012.html","./dracula_013.html","./dracula_014.html","./dracula_015.html","./dracula_016.html","./dracula_017.html","./dracula_018.html","./dracula_019.html","./dracula_020.html","./dracula_021.html","./dracula_022.html","./dracula_023.html","./dracula_024.html","./dracula_025.html","./dracula_026.html","./dracula_027.html","./dracula_028.html","./dracula_029.html","./dracula_030.html","./dracula_031.html","./dracula_032.html","./dracula_033.html","./dracula_034.html","./favicon.png","./index.html","./manifest.json","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/image001_fmt.jpeg","./resources/image002_fmt.jpeg","./resources/obalka_dracula_fmt.jpeg","./resources/upoutavka_eknihy_fmt.jpeg","./scripts/bundle.js","./style/style.min.css","./template-images/circles.png"]));
 }
 
 self.addEventListener('fetch', e => {
